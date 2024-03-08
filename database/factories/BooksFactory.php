27790c18ae36c10e71345ad9771909e285694aa0
Ignacio <italiyankin@gmail.com>
2024-03-08 09:10:21 +0000

    prepare factories and seeders

--- database/factories/BooksFactory.php
+++ database/factories/BooksFactory.php
@@ -17,7 +17,13 @@ class BooksFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => fake()->name(),
+            'isbn' => fake()->isbn10(),
+            'author' => fake()->name(),
+            //'email' => fake()->unique()->safeEmail(),
+            //'email_verified_at' => now(),
+            //'password' => static::$password ??= Hash::make('password'),
+            //'remember_token' => Str::random(10),
         ];
     }
 }
