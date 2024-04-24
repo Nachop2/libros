6df21367545a52cbe948802b9f8e469cb287daa7
Nachop2 <italiyankin@gmail.com>
2024-04-24 16:05:15 +0100

    add auth

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name')->unique();
             $table->enum('role', ['user','admin','superuser']);
-            //$table->string('email')->unique();
+            $table->string('email')->unique();
             //$table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
