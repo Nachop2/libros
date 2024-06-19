90c39c7c5978f5a1ef078faa90a4ed3730ed59fb
M2 <italiyankin@gmail.com>
2024-06-19 15:39:02 +0100

    disable isbn verification

--- app/Http/Requests/StoreBooksRequest.php
+++ app/Http/Requests/StoreBooksRequest.php
@@ -23,7 +23,7 @@ class StoreBooksRequest extends FormRequest
     {
         return [
             'name' => 'required',
-            'isbn' => 'required|regex:/^[0-9]{9}[0-9X]$/',
+            'isbn' => 'required',
             'author' => 'required',
             'imprenta' => 'required',
             'stock' => 'required',
